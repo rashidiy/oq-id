fa1f89db5e8c632e795773d09626be97e4c7bea0
gucci3233 <adkhamjonmakmudov@gmail.com>
2024-10-11 19:06:35 +0500

    refactor: reorganize models directory and rename user model
    - Moved user model to apps/models/auth
    - Added base.py for common models
    - Initialized new test structure

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,1 +1,1 @@
-from .users import *
+from .base import *
---
+++ apps/models/base.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import BigInteger, DateTime
+from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncSession
+from sqlalchemy.orm import (DeclarativeBase, Mapped, declared_attr,
+                            mapped_column)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        name = cls.__name__.lower()
+        if name.endswith('y'):
+            name = name[:-1] + 'ie'
+        return name + 's'
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    @classmethod
+    async def create(cls, session: AsyncSession, **kwargs):
+        instance = cls(**kwargs)
+        session.add(instance)
+        await session.commit()
+        await session.refresh(instance)
+        return instance
