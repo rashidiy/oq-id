0074cb844e9db8ea7fc95585ebdd7c4f8b3dc38e
gucci3233 <adkhamjonmakmudov@gmail.com>
2024-10-12 13:40:34 +0500

    Refactor project structure:
    - Deleted 'apps/models/auth/__init__.py'.
    - Renamed 'user.py' to 'base.py'.
    - Deleted 'apps/models/base.py'.
    - Updated Alembic config and model imports.
    - Cleaned up routing and middleware in 'main.py'.
    - Improved config handling in 'settings/config.py'.
    - Added 'users' router and middleware utils.
    
    This refactor improves organization and sets up for scalability.

--- apps/models/auth/base.py
+++ apps/models/auth/base.py
@@ -1,7 +1,32 @@
+from datetime import datetime
+
 from sqlalchemy import BigInteger, Boolean, Date, ForeignKey, String
+from sqlalchemy import DateTime
+from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncSession
+from sqlalchemy.orm import (DeclarativeBase, declared_attr)
 from sqlalchemy.orm import (Mapped, mapped_column, relationship)
 
-from models import Base
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        name = cls.__name__.lower()
+        if name.endswith('y'):
+            name = name[:-1] + 'ie'
+        return name + 's'
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    @classmethod
+    async def create(cls, session: AsyncSession, **kwargs):
+        instance = cls(**kwargs)
+        session.add(instance)
+        await session.commit()
+        await session.refresh(instance)
+        return instance
 
 
 class User(Base):
@@ -9,10 +34,10 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(String(25), nullable=False)
     phone_number: Mapped[str] = mapped_column(String(13), nullable=False, unique=True)
     password_hash: Mapped[str] = mapped_column(String(100), nullable=False)
-    gender: Mapped[str] = mapped_column(String(6))
-    birth_date: Mapped[Date] = mapped_column(Date)
-    bio: Mapped[str] = mapped_column(String(250))
-    avatar: Mapped[str] = mapped_column(String(250))
+    gender: Mapped[str] = mapped_column(String(6), nullable=True)
+    birth_date: Mapped[Date] = mapped_column(Date, nullable=True)
+    bio: Mapped[str] = mapped_column(String(250), nullable=True)
+    avatar: Mapped[str] = mapped_column(String(250), nullable=True)
     email: Mapped[str] = mapped_column(String(25), unique=True)
     telegram_id: Mapped[str] = mapped_column(BigInteger(), unique=True)
     developer_mode: Mapped[bool] = mapped_column(Boolean, default=False)
