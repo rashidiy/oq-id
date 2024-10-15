f6492357ef8f31478c36c9030e3559e644bc2901
gucci3233 <adkhamjonmakmudov@gmail.com>
2024-10-15 14:18:28 +0500

    auth_forms.py fixed and done more clean code

--- apps/forms/auth_forms.py
+++ apps/forms/auth_forms.py
@@ -1,17 +1,18 @@
 import re
 
 from pydantic import BaseModel, constr, field_validator
-
+from utils.translations import _
 
 def validate_phone_format(phone_number: str) -> str:
     """Validate the phone number format using regex."""
     pattern = r'^(\+998|998|)?\s?(\d{2})\s?(\d{3})\s?(\d{2})\s?(\d{2})$'
     if not re.match(pattern, phone_number):
-        raise ValueError("Invalid phone number format. Use formats like +998 90 123 45 67.")
+        raise ValueError(_("Invalid phone number format. Use formats like +998 xx xxx xx xx."))
     return phone_number
 
 
-class PreRegisterRequest(BaseModel):
+class BaseRequest(BaseModel):
+    """Base model to handle common phone number validation."""
     phone_number: str
 
     @field_validator('phone_number')
@@ -19,48 +20,34 @@ class PreRegisterRequest(BaseModel):
         return validate_phone_format(v)
 
 
-class RegisterRequest(BaseModel):
-    phone_number: str
+class PreRegisterRequest(BaseRequest):
+    """Request model for pre-registration."""
+    pass
+
+
+class RegisterRequest(BaseRequest):
+    """Request model for registration."""
     password: constr(min_length=8, max_length=100)
     verification_code: constr(min_length=5, max_length=5)
 
-    @field_validator('phone_number')
-    def validate_phone_number(cls, v: str) -> str:
-        return validate_phone_format(v)
 
-
-class PreLoginRequest(BaseModel):
-    phone_number: str
+class PreLoginRequest(BaseRequest):
+    """Request model for pre-login."""
     password: constr(min_length=8)
 
-    @field_validator('phone_number')
-    def validate_phone_number(cls, v: str) -> str:
-        return validate_phone_format(v)
 
-
-class LoginRequest(BaseModel):
-    phone_number: str
+class LoginRequest(BaseRequest):
+    """Request model for login."""
     password: constr(min_length=8)
     verification_code: constr(min_length=5, max_length=5)
 
-    @field_validator('phone_number')
-    def validate_phone_number(cls, v: str) -> str:
-        return validate_phone_format(v)
+
+class PreResetPassword(BaseRequest):
+    """Request model for pre-resetting the password."""
+    pass
 
 
-class PreResetPassword(BaseModel):
-    phone_number: str
-
-    @field_validator('phone_number')
-    def validate_phone_number(cls, v: str) -> str:
-        return validate_phone_format(v)
-
-
-class ResetPassword(BaseModel):
-    phone_number: str
+class ResetPassword(BaseRequest):
+    """Request model for resetting the password."""
     password: constr(min_length=8)
     verification_code: constr(min_length=5, max_length=5)
-
-    @field_validator('phone_number')
-    def validate_phone_number(cls, v: str) -> str:
-        return validate_phone_format(v)
