07ca19e139b2eabbd23cfdb3df5f93debc6db512
gucci3233 <adkhamjonmakmudov@gmail.com>
2024-10-15 14:54:47 +0500

    removed users.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from fastapi import FastAPI
 from routers.auth.login import router as login_router
 from routers.auth.register import router as register_router
 from routers.auth.reset_password import router as reset_password
-from routers.users import router as user_router
 from utils.middlewares.middlewares import init_middlewares
 
 BASE_DIR = os.path.dirname(__file__)
@@ -19,10 +18,8 @@ app = FastAPI(
 )
 
 init_middlewares(app)
-app.include_router(user_router, prefix="/api/v1", tags=["Users"])
 app.include_router(register_router, prefix="/api/v1", tags=["Auth"])
 app.include_router(login_router, prefix="/api/v1", tags=["Auth"])
-
 app.include_router(reset_password, prefix="/api/v1", tags=["Auth"])
 
 if __name__ == "__main__":
