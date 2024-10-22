5f4ba3292150926ad96ca4471f42f8bfce5294d4
gucci3233 <adkhamjonmakmudov@gmail.com>
2024-10-22 13:45:11 +0500

    added TokenManager and fixed imports

--- apps/routers/auth/register.py
+++ apps/routers/auth/register.py
@@ -5,7 +5,7 @@ from fastapi import HTTPException, status
 from forms.auth import PreRegisterRequest, RegisterRequest
 from models.users import User
 from settings.config import Config
-from utils.services import OTPManager, AuthService, create_access_token, create_refresh_token
+from utils.services import OTPManager, AuthService, TokenManager
 from utils.translations import trans as _
 from .base import router
 
@@ -66,8 +66,8 @@ async def register(data: RegisterRequest):
     user = await User.create_user(data.phone_number, data.password)
     await OTPManager.delete_otp(data.phone_number, "register")
     access_token_expires = timedelta(minutes=Config.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.phone_number}, expires_delta=access_token_expires)
-    refresh_token = create_refresh_token(data={"sub": user.phone_number})
+    access_token = TokenManager.create_access_token(data={"sub": user.phone_number}, expires_delta=access_token_expires)
+    refresh_token = TokenManager.create_refresh_token(data={"sub": user.phone_number})
 
     return {
         "success": True,
--- apps/utils/services/__init__.py
+++ apps/utils/services/__init__.py
@@ -1,3 +1,3 @@
 from .otp import OTPManager
-from .jwt import create_refresh_token, create_access_token, verify_token
+from .jwt import TokenManager
 from.auth import AuthService
