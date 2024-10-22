a03ed13a3e0ee5f502059f7f092844a63c436828
gucci3233 <adkhamjonmakmudov@gmail.com>
2024-10-22 13:46:01 +0500

    added redis conf

--- settings/config.py
+++ settings/config.py
@@ -8,9 +8,19 @@ from redis.asyncio import Redis
 class Config:
     SECRET_KEY: str = os.getenv('SECRET_KEY')
     JWT_ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 2
+    ACCESS_TOKEN_EXPIRE_MINUTES = 15
+    REDIS_HOST: str = os.getenv('REDIS_HOST', 'localhost')
+    REDIS_PORT: int = int(os.getenv('REDIS_PORT', 6379))
+    REDIS_DB: int = int(os.getenv('REDIS_DB', 0))
+    REDIS_PASSWORD: str = os.getenv('REDIS_PASSWORD', None)
 
 
-redis_client = Redis(host='localhost', port=6379, db=0, decode_responses=True)
+redis_client = Redis(
+    host=Config.REDIS_HOST,
+    port=Config.REDIS_PORT,
+    db=Config.REDIS_DB,
+    password=Config.REDIS_PASSWORD,
+    decode_responses=True
+)
 
 conf = Config()
