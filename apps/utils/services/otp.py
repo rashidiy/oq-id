a53452dbe2ba23ccd72064817c2f115e50d9b411
gucci3233 <adkhamjonmakmudov@gmail.com>
2024-10-30 21:00:51 +0500

    deleted unbeatiful codes and fixed more optimal way

--- apps/utils/services/otp.py
+++ apps/utils/services/otp.py
@@ -5,12 +5,6 @@ from settings.config import redis_client
 
 class OTPManager:
     """Handles OTP generation, storage, and validation."""
-    REFRESH_TOKEN_EXPIRE_DAYS = 7
-    OTP_EXPIRATION_MINUTES = 2
-    OTP_REQUEST_LIMIT = 1
-    OTP_REQUEST_TIME_WINDOW = 60
-    OTP_REDIS_KEY_TEMPLATE = "otp:{key}"
-    RATE_LIMIT_REDIS_KEY_TEMPLATE = "otp_rate_limit:{key}"
 
     @classmethod
     async def generate_otp(cls) -> str:
@@ -21,7 +15,7 @@ class OTPManager:
     async def store_otp(cls, key: str, otp: str, action: str):
         """Stores OTP in Redis with an expiration time."""
         key = cls._build_key(key, action)
-        await redis_client.setex(key, cls.OTP_EXPIRATION_MINUTES * 60, otp)
+        await redis_client.setex(key, 90, otp)
 
     @classmethod
     async def get_otp(cls, key: str, action: str) -> str:
@@ -36,31 +30,6 @@ class OTPManager:
         await redis_client.delete(key)
 
     @classmethod
-    async def is_rate_limit_exceeded(cls, key: str, action: str) -> tuple:
-        """Checks if the OTP request rate limit is exceeded."""
-        key = cls._build_rate_limit_key(key, action)
-        request_count = await redis_client.get(key)
-
-        if request_count and int(request_count) >= cls.OTP_REQUEST_LIMIT:
-            expiration_time = await redis_client.ttl(key)
-            return True, expiration_time
-        return False, None
-
-    @classmethod
-    async def increment_rate_limit(cls, key: str, action: str):
-        """Increments the OTP request count with a rate limit."""
-        key = cls._build_rate_limit_key(key, action)
-        if not await redis_client.exists(key):
-            await redis_client.set(key, 1, ex=cls.OTP_REQUEST_TIME_WINDOW)
-        else:
-            await redis_client.incr(key)
-
-    @classmethod
     def _build_key(cls, key: str, action: str) -> str:
         """Helper method to build Redis key for OTP."""
         return f"{action}_otp:{key}"
-
-    @classmethod
-    def _build_rate_limit_key(cls, key: str, action: str) -> str:
-        """Helper method to build Redis key for rate limiting."""
-        return f"{action}_rate_limit:{key}"
